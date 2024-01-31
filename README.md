684361f2181e7a1579ee1c4aa2409e6980a604e3
sam <abluecar@gmail.com>
2024-01-30 20:02:13 -0500

    feat: Remove book, view info, mark read
    Finish project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-update me
+# Project: Library (The Odin Project)
+
+[Link to this project's specs](https://www.theodinproject.com/lessons/javascript-library)
+
+Description: In this project we will make a simple webpage that can keep track
+of books which we have to manually add. It will be good practice for learning JavaScript
+constructors, prototypes, and HTML forms.
+
+Tech used: JavaScript, HTML, and CSS
---
+++ dialog.js
@@ -0,0 +1,29 @@
+const addBookDialog = document.getElementById("add-book-dialog");
+const addButton = document.getElementById("add-btn");
+const closeButton = document.getElementById("close-btn");
+const form = document.getElementById("form");
+
+addButton.addEventListener("click", () => {
+  addBookDialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  addBookDialog.close();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevent form from trying to submit to a server
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").value;
+
+  if (title == "" || author == "" || pages == "" || read == "")
+    return alert("Some items were left blank.");
+
+  myLibrary.push(new Book(title, author, pages, read));
+  form.reset();
+  addBookDialog.close();
+  showBooks();
+});
+
